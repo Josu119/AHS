faf3f2b6eefa1da99c7ca661d702be563c27d326
Harry-Reyes <114123042+Harry-Reyes@users.noreply.github.com>
2023-12-27 20:43:12 +0800

    tailwind css integration

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Apex Hub Society</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    @yield('content')
+    <h1 class="font-bold text-3xl text-center text-congressBlue">Apex Hub Society</h1>
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        // origin color
+        mainBlue: 'hsl(208, 60%, 33%)',
+
+        // saturated colors
+        darkOlive: '#2d4706',
+        greenLeaf: '#4c6e00',
+        congressBlue: '#004786',
+        darkRaspberry: '#861c53',
+        mulberryWood: '#5c0b30',
+
+        // lighter colors
+        greenOnion: '#77a028',
+        androidGreen: '#a3cb49',
+        cornflowerBlue: '#539fe3',
+        cadillacPink: '#e382b5',
+        bashfulPink: '#b6598b',
+
+        // technical colors
+        dangerRed: '#c73737',
+        cautionYellow: '#f8b800',
+        safeGreen: '#57b03b'
+      }
+    },
+  },
+  plugins: [],
+}
