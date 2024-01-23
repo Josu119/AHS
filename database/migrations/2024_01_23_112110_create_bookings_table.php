832152edebefd11ef0e4b700eb2e9224721e8c59
Harry-Reyes <114123042+Harry-Reyes@users.noreply.github.com>
2024-01-23 19:46:02 +0800

    added database schema

---
+++ database/migrations/2024_01_23_112110_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            // ! CHECK nullable(), cascadeOnDelete(), cascadeOnUpdate(), AND nullOnDelete() IF ANY DATABASE-RELATED ERRORS/BUGS OCCUR
+            $table->foreignId('user_id')
+                  ->nullable()
+                  ->constrained(table: 'users', indexName: 'bookings_user_id')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate()
+                  ->nullOnDelete();
+            $table->foreignId('desk_id')
+                  ->nullable()
+                  ->constrained(table: 'desks', indexName: 'bookings_desk_id')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate()
+                  ->nullOnDelete();
+            $table->date('date');
+            $table->boolean('is_canceled');
+            $table->boolean('is_archived');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
