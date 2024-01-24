ad7b62e197af5c6646981167faa901c00bfb85d7
Harry-Reyes <114123042+Harry-Reyes@users.noreply.github.com>
2024-01-24 08:52:02 +0800

    modified userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,8 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Illuminate\Validation\Rules\Enum;
-use PhpParser\Node\Expr\Cast\Bool_;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
