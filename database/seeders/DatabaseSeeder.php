35a92f5b9d532d1ce31dfc45758d52a9c9f33064
Harry-Reyes <114123042+Harry-Reyes@users.noreply.github.com>
2024-01-28 21:29:46 +0800

    added desk factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,18 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
+        \App\Models\Desk::factory(33)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        for ($i=1; $i <= 16; $i++) {
-            \App\Models\Desk::create([
-                'desk_number' => $i,
-                'is_out_of_order' => 0,
-                'is_available' => 1,
-            ]);
-        }
-
     }
 }
