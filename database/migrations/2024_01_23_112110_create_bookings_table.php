40385712d3c6e0569742c57913b26de31d33126c
Harry-Reyes <114123042+Harry-Reyes@users.noreply.github.com>
2024-01-29 17:46:05 +0800

    updated bookings db migration

--- database/migrations/2024_01_23_112110_create_bookings_table.php
+++ database/migrations/2024_01_23_112110_create_bookings_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            // ! CHECK nullable(), cascadeOnDelete(), cascadeOnUpdate(), AND nullOnDelete() IF ANY DATABASE-RELATED ERRORS/BUGS OCCUR
+            // CHECK nullable(), cascadeOnDelete(), cascadeOnUpdate(), AND nullOnDelete() IF ANY DATABASE-RELATED ERRORS/BUGS OCCUR (removed)
             $table->foreignId('user_id')
-                  ->nullable()
-                  ->constrained(table: 'users', indexName: 'bookings_user_id')
+                  ->constrained(/*table: 'users', indexName: 'bookings_user_id'*/)
                   ->cascadeOnDelete();
             $table->foreignId('desk_id')
-                  ->nullable()
-                  ->constrained(table: 'desks', indexName: 'bookings_desk_id')
+                  ->constrained()
                   ->cascadeOnDelete();
             $table->date('date');
             $table->boolean('is_canceled')->default('0');
